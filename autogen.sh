262164bfb84957a8668a10cdae5cb635e325c8e4
Srinivasa Ragavan <sragavan@gnome.org>
2012-02-08 08:01:07 +0530

    Use GNOME's autogen.sh script.

--- autogen.sh
+++ autogen.sh
@@ -1,12 +1,26 @@
 #!/bin/sh
-[ -e config.cache ] && rm -f config.cache
+# Run this to generate all the initial makefiles, etc.
 
-libtoolize --automake
+srcdir=`dirname $0`
+test -z "$srcdir" && srcdir=.
 
-aclocal
-autoconf
-autoheader
-automake -a
-./configure $@
-exit
+PKG_NAME="e-mail-factory"
+REQUIRED_AUTOCONF_VERSION=2.58
+REQUIRED_AUTOMAKE_VERSION=1.10
+REQUIRED_LIBTOOL_VERSION=2.2
+REQUIRED_INTLTOOL_VERSION=0.35.5
 
+(test -f $srcdir/configure.ac \
+  && test -f $srcdir/ChangeLog \
+  && test -d $srcdir/src) || {
+    echo -n "**Error**: Directory "\`$srcdir\'" does not look like the"
+    echo " top-level $PKG_NAME directory"
+    exit 1
+}
+
+which gnome-autogen.sh || {
+    echo "You need to install gnome-common from the GNOME CVS"
+    exit 1
+}
+
+USE_GNOME2_MACROS=1 . gnome-autogen.sh
